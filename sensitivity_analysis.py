8b11b2e04ebf0e492d155a006adad282c4ff14a5
Clément Adandé <132694770+clemsadand@users.noreply.github.com>
2025-06-12 00:56:44 +0100

    Add files via upload

--- sensitivity_analysis.py
+++ sensitivity_analysis.py
@@ -22,21 +22,13 @@ domain = pinn.Domain()
 
 domain_size = 20
 n_steps = 10
-mesh_size = 64
-
-# Create mesh only once
-mesh_file = crbe.create_mesh(mesh_size, domain_size=domain_size)
-mesh = meshio.read(mesh_file)
-mesh_data = crbe.MeshData(mesh, domain, nt=n_steps)
+#mesh_size = 64
 
 # PINN Hyperparamters
-n_ic = round(0.2 * mesh_data.number_of_segments)
-n_bc = n_ic
-n_col = mesh_data.number_of_segments - n_ic - n_bc
-batch_sizes = {'pde': n_col, 'ic': n_ic, 'bc': n_ic}
+
 lambda_weights = {'pde': 1.0, 'ic': 10.0, 'bc': 10.0}
-layers = [3, 20, 1]#20, 20, 20, 20, 1]
-epochs = 2
+layers = [3] + [64]*4 + [1]
+epochs = 10000
 lr = 1e-3
 
 # --- Function to track CPU memory ---
@@ -47,35 +39,52 @@ def get_cpu_memory():
 D_list = [0.001, 0.01, 0.1, 1.0, 10]
 sensitivity_data = []
 
-for i, D in enumerate(D_list):
-    print(f"Running for D = {D}")
-    #PINN's setup
-    pproblem = pinn.Problem(D=D)
-    model = pinn.PINN(layers, pproblem, domain).to(device)
-    model.train(batch_sizes, epochs, lr, lambda_weights, early_stopping_patience=50, early_stopping_min_delta=1e-6)
-    pinn_rel_l2_error, pinn_l2_error, pinn_max_error = model.compute_errors(mesh_data, pproblem.analytical_solution)
-    
-    print()
-    
-    #CR-BE setup
-    cproblem = crbe.Problem(D=D)
-    solver = crbe.BESCRFEM(domain, cproblem, mesh_data, crbe.ElementCR(), 1)
-    solver.solve()
+filename = "experimental_results/df_sensitivity_data.csv"
 
-    crbe_rel_l2_error, crbe_l2_error, crbe_max_error = solver.compute_errors(cproblem.analytical_solution)
-    
-    sensitivity_data.append({
-        "diffusion_coef": D,
-        "pinn_l2_error": pinn_rel_l2_error,
-        "max_error": pinn_max_error,
-        "cr_l2_error": crbe_rel_l2_error,
-        "cr_max_error": crbe_max_error,
-    })
-    print("="*50)
+mesh_sizes = [4, 8, 16, 32, 64]
+
+for mesh_size in mesh_sizes:
+	print(f"Training for mesh size {mesh_size} ...")
+	
+	# Create mesh only once
+	mesh_file = crbe.create_mesh(mesh_size, domain_size=domain_size)
+	mesh = meshio.read(mesh_file)
+	mesh_data = crbe.MeshData(mesh, domain, nt=n_steps)
+	n_ic = round(0.2 * mesh_data.number_of_segments)
+	n_bc = n_ic
+	n_col = mesh_data.number_of_segments - n_ic - n_bc
+	batch_sizes = {'pde': n_col, 'ic': n_ic, 'bc': n_ic}
+	
+	for i, D in enumerate(D_list):
+		print(f"Running for D = {D}")
+		#PINN's setup
+		pproblem = pinn.Problem(D=D)
+		model = pinn.PINN(layers, pproblem, domain).to(device)
+		model.train(batch_sizes, epochs, lr, lambda_weights, early_stopping_patience=50, early_stopping_min_delta=1e-6)
+		pinn_rel_l2_error, pinn_l2_error, pinn_max_error = model.compute_errors(mesh_data, pproblem.analytical_solution)
+		
+		print()
+		
+		#CR-BE setup
+		cproblem = crbe.Problem(D=D)
+		solver = crbe.BESCRFEM(domain, cproblem, mesh_data, crbe.ElementCR(), 1)
+		solver.solve()
+
+		crbe_rel_l2_error, crbe_l2_error, crbe_max_error = solver.compute_errors(cproblem.analytical_solution)
+		
+		sensitivity_data.append({
+		    "mesh_size": mesh_size,
+		    "diffusion_coef": D,
+		    "pinn_l2_error": pinn_rel_l2_error,
+		    "max_error": pinn_max_error,
+		    "cr_l2_error": crbe_rel_l2_error,
+		    "cr_max_error": crbe_max_error,
+		})
+		print()
+		print("="*50)
 
 df_sensitivity_data = pd.DataFrame(sensitivity_data)
 
-filename = "experimental_results/df_sensitivity_data.csv"
 df_sensitivity_data.to_csv(filename)
 
 print(f"Sensitivity analysis ended and results are saved at {filename}")
